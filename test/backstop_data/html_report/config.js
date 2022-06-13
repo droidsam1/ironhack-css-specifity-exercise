f33cf8a03c45fb9764803e1fc4212f7f9dbab9b6
Samuel Taboas <droidsam@gmail.com>
2022-06-13 18:49:10 +0200

    added backstop init and approving changes

---
+++ test/backstop_data/html_report/config.js
@@ -0,0 +1,30 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_backstop_test_index_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20220613-184825\\backstop_default_backstop_test_index_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_backstop_test_index_0_document_0_tablet.png",
+        "label": "backstop test index",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:5501/index.html",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
